07d0d92b7fbc69155a456bee45c961f22a403473
prateekh45 <61591344+prateekh45@users.noreply.github.com>
2020-08-31 17:00:31 +0530

    added new files

---
+++ resources/js/scripts.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+    
+    
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+    
+     /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }        
+    });
+     $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
+      /* Scroll on buttons */
+    $('.js--scroll-to-about').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-events').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-event').offset().top}, 1000); 
+    });
+     $('.js--scroll-to-home').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-home').offset().top}, 1000); 
+    });
+    
+     /*...........animation on scroll...........*/
+     $('.js--wp-0').waypoint(function(direction) {
+        
+        $('.js--wp-0').addClass('animated fadeInUp');
+    });
+    
+    $('.js--wp-1').waypoint(function(direction) {
+        
+        $('.js--wp-1').addClass('animated fadeInUp');
+    }, {
+        offset:'70%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        
+        $('.js--wp-2').addClass('animated  fadeInUp');
+    }, {
+        offset:'70%'
+    });
+    
+      $('.js--wp-3').waypoint(function(direction) {
+        
+        $('.js--wp-3').addClass('animated fadeInUp');
+    }, {
+        offset:'70%'
+    });
+    
+      $('.js--wp-4').waypoint(function(direction) {
+        
+        $('.js--wp-4').addClass('animated fadeInUp');
+    }, {
+        offset:'70%'
+    });
+    
+    });
